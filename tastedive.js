380d21faf82a025c7287b57f602a90fd120e8cf3
Muhammad Naeem Tariq <admin@softwareprince.com>
2021-11-17 07:18:20 +0500

    new repo

--- tastedive.js
+++ tastedive.js
@@ -5,6 +5,7 @@ setInterval(() => {
         let n = document.getElementsByClassName("button-entity-action button-entity-action--add-to-list-white")[0]
         n.href = "https://lookmovie.io/movies/search/?q=" + m
         n.target = "_blank"
+        n.innerText = "Look Movie"
         console.log("success");
 
     } catch (e) {
